9092411bb4b6f161eda065207c9d4d80cf6cebd9
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-01-12 03:40:04 +0200

    Comparison between 2 alternative dairy products

--- Question/Question_1/Question_1.py
+++ Question/Question_1/Question_1.py
@@ -4,6 +4,7 @@ import pandas as pd
 # import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+import seaborn as sys
 
 
 
@@ -12,6 +13,8 @@ if __name__ == '__main__':
     pd.set_option('display.max_rows', 5000)
     df = pd.read_csv('/home/shay_diy/PycharmProjects/digging_deep_into_Israel_prices/Data/israel_prices.csv')
     df = df.replace(np.nan, '', regex=True)
+
+    plt.subplot(1, 2, 1)
 
     # In order to concanate the 2 column of the "Year" and "Month" we need to convert to string if needed
     df['Year'] = df['Year'].astype(str)
@@ -36,15 +39,36 @@ if __name__ == '__main__':
     print('*')
 
     # Plotting the line chart
-
+    x_values = data_before_presenting['Date']
     # First line :
-    plt.plot(data_before_presenting['Date'], data_before_presenting['price'] , label='Soft white cheese ')
+    plt.subplot(1, 2, 1)
+    #plt.style.use('fivethirtyeight')
+    plt.plot(x_values, data_before_presenting['price'] , label='Soft white cheese ',linewidth=3.5 , color='navy' )
+    plt.fill_between(x_values, data_before_presenting['price'], color='skyblue', alpha=0.4, label='Filled Area')
+
+    plt.title('Soft white cheese price over the years',fontname='Franklin Gothic Medium Cond', color= 'gray', fontsize=21)
+
     # Second line :
-    plt.plot(data_before_presenting['Date'], data_before_presenting["Cottage cheese' (250 grams)"], label='Cottage_cheese')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(x_values, data_before_presenting["Cottage cheese' (250 grams)"], label='Cottage_cheese', linewidth=4 ,color='green')
+    plt.fill_between(x_values, data_before_presenting['price'], color='lightgreen', alpha=0.4, label='Filled Area')
+    plt.title('Cottage cheese price over the years',fontname='Franklin Gothic Medium Cond', color= 'gray',fontsize=21)
+
+    plt.tick_params(bottom=False, top=False,
+                   left=False, right=False)
+
+    # Remove the frame (spines) from the second subplot
+    plt[0,1].spines['top'].set_visible(False)
+    # ax2.spines['right'].set_visible(False)
+    # ax2.spines['bottom'].set_visible(False)
+    ax2.spines['left'].set_visible(False)
+
     # Adding labels and title
+
     plt.xlabel('Year')
     plt.ylabel('Price')
-    plt.title('Prices of Soft white cheese VS Cottage cheese \n over the last 2 decades' ,fontsize=30,weight='bold',fontname='Franklin Gothic Medium Cond')
+    plt.suptitle('Comparison between 2 alternative dairy products' ,fontsize=29,weight='bold',fontname='Franklin Gothic Medium Cond')
 
 
     # Adding a legend
