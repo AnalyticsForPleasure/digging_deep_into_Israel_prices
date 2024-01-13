719142f7cffd4d3196180bff48c40b0ea7d0e759
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-01-13 21:49:02 +0200

    starting with the creation of the box plot  data

--- Question/Question_1/Question_1.py
+++ Question/Question_1/Question_1.py
@@ -14,62 +14,44 @@ if __name__ == '__main__':
     df = pd.read_csv('/home/shay_diy/PycharmProjects/digging_deep_into_Israel_prices/Data/israel_prices.csv')
     df = df.replace(np.nan, '', regex=True)
 
-    plt.subplot(1, 2, 1)
 
-    relavent_years = np.arange(2006, 2023, 2)
 
-    filter_data_by_month= df.loc[df['Month'] == 1, :]
-    filter_data_by_month_year = filter_data_by_month[filter_data_by_month['Year'].isin(relavent_years)]
+    column_headers = list(df.columns.values)
+    print("The Column Header :", column_headers)
+
+
+    #step 1: filtering the years
+    new_data_by_years = df.loc[df['Year'] >= 1987]
+    #df.loc[df['player_name'] == 'Chris Paul', :]
     print('*')
 
 
-    data = filter_data_by_month_year.filter(regex='Cottage|Soft white|Year') # 'Soft white cheese (250 grams)', Cottage cheese' (250 grams)'
+
+
+    filter_vegi_and_fruits = ['Apples - Golden Delicious (1 kg)',
+                    'Apples - Granny Smith (1 kg)',
+                    'Apples - Jonathan (1 kg)',
+                    'Avocado (1 kg)',
+                    'Oranges - Valencia (1 kg)',
+                    'Oranges - smutty (1 kg)',
+                    'Light green pepper (1 kg)',
+                    'Dark green pepper (1 kg)',
+                    'Melons - Galia (1 kg)',
+                    'Green bins (1 kg)',
+                    'Carrot (1 kg)',
+                    'Green grapes without pits (1 kg)',
+                    'lemons (1 kg)',
+                    'Cucumbers (1 kg)']
+                    #'Tomatoes'
+
+    # Step 2: filtering the data by specific data I need
+    column_indices = [new_data_by_years.columns.get_loc(col) for col in filter_vegi_and_fruits]
+    full_relevant_data = new_data_by_years.iloc[:, column_indices]
     print('*')
 
-    # Plotting the line chart
-    x_values = list(data.loc[:,'Year'])
-    y_values_white_cheese = list(data.loc[:,'Soft white cheese (250 grams)'])
-    y_values_cottage = list(data.loc[:, "Cottage cheese' (250 grams)"])
-    print('*')
-    # First line :
-    plt.subplot(1, 2, 1)
-    #plt.style.use('fivethirtyeight')
-    plt.plot(x_values, y_values_white_cheese , label='Soft white cheese ',linewidth=3.5 , color='navy' )
-    plt.fill_between(x_values, y_values_white_cheese, color='skyblue', alpha=0.4, label='Filled Area')
-
-    plt.title('Soft white cheese price over the years',fontname='Franklin Gothic Medium Cond', color= 'gray', fontsize=21)
-
-    # Second line :
-
-    plt.subplot(1, 2, 2)
-    plt.plot(x_values,y_values_cottage , label='Cottage_cheese', linewidth=4 ,color='green')
-    plt.fill_between(x_values, y_values_cottage, color='lightgreen', alpha=0.4, label='Filled Area')
-    plt.title('Cottage cheese price over the years',fontname='Franklin Gothic Medium Cond', color= 'gray',fontsize=21)
-
-    plt.tick_params(bottom=False, top=False,
-                   left=False, right=False)
-
-    # Remove the frame (spines) from the second subplot
-    # plt[0,1].spines['top'].set_visible(False)
-    # # ax2.spines['right'].set_visible(False)
-    # # ax2.spines['bottom'].set_visible(False)
-    # ax2.spines['left'].set_visible(False)
-
-    # Adding labels and title
-
-    plt.xlabel('Year')
-    plt.ylabel('Price')
-    plt.suptitle('Comparison between Cottage Cheese and Soft White Cheese as Substitute Products' ,fontsize=29,weight='bold',fontname='Franklin Gothic Medium Cond')
 
 
-    # Adding a legend
-    #plt.legend()
-
-    plt.show()
 
 
-    # 'Milk substitutes for babies(400 grams)', 'Milk substitutes for babies (450 grams)', 'Milk substitutes for babies (700 grams)'
-    # 'Margarine in a package (200 grams)'
-    # 'Margarine for spreading in a cup (250 grams)', 'Margarine in a package (200 grams)'
-    # 'Soft white cheese (250 grams)'
-    # 'Cottage cheese (250 grams)'
+
+
