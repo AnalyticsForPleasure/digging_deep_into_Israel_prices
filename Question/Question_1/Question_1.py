7a4534813feeae3d638e3c000ddfa410f3594e67
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-01-16 18:49:27 +0200

    Adding - subplot chart for 2015+2020 - working perfectly

--- Question/Question_1/Question_1.py
+++ Question/Question_1/Question_1.py
@@ -1,7 +1,5 @@
-import numpy as np
 import pandas as pd
-# import dataframe_image as dfi
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 #import seaborn as sys
@@ -21,16 +19,16 @@ def creating_the_relavent_data(df):
     print('*')
     filter_vegi_and_fruits = ['Apples - Golden Delicious (1 kg)',
                               'Apples - Granny Smith (1 kg)',
-                              #'Apples - Jonathan (1 kg)',
+                              # 'Apples - Jonathan (1 kg)',
                               'Avocado (1 kg)',
-                              #'Oranges - Valencia (1 kg)',
+                              # 'Oranges - Valencia (1 kg)',
                               'Oranges - smutty (1 kg)',
-                              #'Light green pepper (1 kg)',
-                              #'Dark green pepper (1 kg)',
-                              #'Melons - Galia (1 kg)',
+                              # 'Light green pepper (1 kg)',
+                              # 'Dark green pepper (1 kg)',
+                              # 'Melons - Galia (1 kg)',
                               'Green bins (1 kg)',
                               'Carrot (1 kg)',
-                              #'Green grapes without pits (1 kg)',
+                              # 'Green grapes without pits (1 kg)',
                               'lemons (1 kg)',
                               'Cucumbers (1 kg)']
 
@@ -50,14 +48,11 @@ def creating_the_relavent_data(df):
     res = traspose_data.rename(columns=dict(zip(old_names, new_names)), inplace=False)
     res = res.reset_index()
     res = res.rename(columns={res.columns[0]: 'Item_Name'})
-    res =res.reset_index()
+    res = res.reset_index()
     res = res.drop('index', axis=1)
 
     # Remove "(1 kg)" from the 'Item_Name' column
     res['Item_Name'] = res['Item_Name'].apply(lambda x: x.replace('(1 kg)', ''))
-
-    print('*')
-
 
     return res
 
@@ -66,37 +61,42 @@ def creating_the_relavent_data(df):
 # input:
 # return value:
 # ****************************************************************************************************************
-def plotting_advance_bar_plot(relevant_data_sorted,current_range_of_year,current_palette):
-    plt.figure(figsize=(4, 6))
+def plotting_advance_bar_plot(res):
 
-    relevant_data_sorted = relevant_data_sorted.sort_values(by=current_range_of_year, ascending=False)
-    relevant_data_sorted = relevant_data_sorted.reset_index()
-    relevant_data_sorted = relevant_data_sorted.drop('index', axis=1)
-    print('*')
-    # y_value= relevant_data_sorted.loc[:,'Item_Name']
-    # x_value= relevant_data_sorted.loc[:,current_range_of_year]
-    #
-    # ax = sns.barplot(data=relevant_data_sorted, y=y_value, x=[100] * len(relevant_data_sorted), color='lightgrey',saturation=1) # y='Item_Name'
-    # sns.barplot(data=relevant_data_sorted, y=y_value , x=x_value ,palette=current_palette , saturation=1, ax=ax) # color='skyblue' # y='Item_Name', x='Price_change_2020' Blues , flare , crest
+    fig, axes = plt.subplots(1, 2, figsize=(15, 4))
+    list_of_colors = ['royalblue','deepskyblue']
+    list_of_ranges_of_years = ['2015', '2020']
 
-    plt.figure(figsize=(6, 6))
-    ax = sns.barplot(data=relevant_data_sorted, y=relevant_data_sorted.loc[:,'Item_Name'], x=[100] * len(relevant_data_sorted), color='lightgrey', saturation=1)
-    sns.barplot(data=relevant_data_sorted, y=relevant_data_sorted.loc[:,'Item_Name'], x=relevant_data_sorted.loc[:,current_range_of_year], color='skyblue', saturation=1, ax=ax)
-    for lbl in ax.get_yticklabels():
-        # add the y tick labels as right aligned text into the plot
-        ax.text(0.985, lbl.get_position()[1], lbl.get_text(), transform=ax.get_yaxis_transform(), ha='right',
-                va='center', fontname='Franklin Gothic Medium Cond', fontsize=16, fontweight='bold')
-    ax.bar_label(ax.containers[1], fmt=' %.2f %%', fontname='Franklin Gothic Medium Cond', fontsize=14,
-                 fontweight='bold')  # label the bars
-    ax.set_xticks([])  # remove the x ticks
-    ax.set_yticks([])  # remove the y ticks
-    ax.set_xlabel('')  # remove the x label
-    ax.set_ylabel('')  # remove the y label
-    ax.margins(x=0)  # remove the spacing at the right
-    sns.despine(left=True, bottom=True)  # remove the spines
+    for idx, (current_range_of_year, current_color) in enumerate(zip(list_of_ranges_of_years, list_of_colors)):
+        new_df = res.loc[:, ['Item_Name', current_range_of_year]].sort_values(by=current_range_of_year,ascending=False)
+        print('*')
+        ax = sns.barplot(data=new_df,y=new_df.loc[:, 'Item_Name'], x=[100] * len(new_df),color='lightgrey', saturation=1, ax=axes[idx])
+        sns.barplot(data=new_df, y=new_df.loc[:, 'Item_Name'],x=new_df.loc[:, current_range_of_year], color=current_color, saturation=1, ax=ax)
+        for lbl in ax.get_yticklabels():
+            ax.text(0.985, lbl.get_position()[1], lbl.get_text(), transform=ax.get_yaxis_transform(),
+                    ha='right', va='center', fontname='Franklin Gothic Medium Cond', fontsize=14, fontweight='bold')
+        ax.bar_label(ax.containers[1], fmt=' %.2f %%', fontname='Franklin Gothic Medium Cond', fontsize=14,
+                         fontweight='bold')  # label the bars
+
+        ax.set_xticks([])  # remove the x ticks
+        ax.set_yticks([])  # remove the y ticks
+        ax.set_xlabel('')
+        ax.set_ylabel('')  # remove the y label
+        ax.margins(x=0)  # remove the spacing at the right
+        sns.despine(left=True, bottom=True)  # remove the spines
+
+        # plt.title(f"Increase in vegetable & fruits prices\n from {int(current_range_of_year) - 5} to {current_range_of_year}",
+        #           loc='center', fontproperties='Franklin Gothic Medium Cond', size=20, color='slategray', pad=10 )
+        ax.set_title(f"Increase in prices {int(current_range_of_year) - 5} to {current_range_of_year}", loc='center',
+                     size=14, color='slategray', pad=10,fontname='Franklin Gothic Medium Cond', fontsize=16)
+
     plt.tight_layout()
-    plt.title(f"Increase in vegetable & fruits prices\n from {int(current_range_of_year) - 5} to {current_range_of_year}", loc='center',fontproperties='Franklin Gothic Medium Cond', size=24, color='slategray', pad=30)  # }
-    plt.savefig(f'plotting_advance_bar_plot_year_{current_range_of_year}.jpg', dpi=250, bbox_inches='tight')
+    fig.suptitle("Ten-Year Price Trends in Vegetables and Fruits", fontweight='bold',fontproperties='Franklin Gothic Medium Cond', size=26)
+
+    #plt.title(f"Increase in vegetable & fruits prices",loc='center', fontproperties='Franklin Gothic Medium Cond', size=20, color='slategray', pad=10 )
+
+    plt.savefig(f'plotting_advance_bar_plot_year.jpg', dpi=250, bbox_inches='tight')
+
     plt.show()
 
 if __name__ == '__main__':
@@ -105,19 +105,15 @@ if __name__ == '__main__':
     df = pd.read_csv('/home/shay_diy/PycharmProjects/digging_deep_into_Israel_prices/Data/israel_prices.csv')
     df = df.replace(np.nan, '', regex=True)
 
-    list_of_ranges =  ['2005','2010','2015', '2020']
-    list_of_palettes = ['rocket_r','crest','flare','Blues']
+    #list_of_ranges = [['2015', '2020'], ['2005','2010']]
 
     column_headers = list(df.columns.values)
     print("The Column Header :", column_headers)
 
+
     res = creating_the_relavent_data(df)
-
-    for current_range_of_year, current_palette in zip(list_of_ranges,list_of_palettes) :
-
-        new_df= res.loc[:, ['Item_Name', current_range_of_year]]
-        plotting_advance_bar_plot(new_df,current_range_of_year,current_palette)
-        print('*')
+    plotting_advance_bar_plot(res)
+    print('*')
 
 
 
