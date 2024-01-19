09cbe2c2897e2acd8bddae025cf4556b6dbfc987
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-01-19 15:22:41 +0200

    need to fix the second function

--- Question/Question_3/Question_3.py
+++ Question/Question_3/Question_3.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 # **************************************************************************************************************
 # Function  name: creating_the_data_food_item_prices_by_percentage_change_range_of_years
@@ -12,7 +12,6 @@ import seaborn as sns
 def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, ending_year, starting_year):
 
     filter_data_by_year = df.loc[(df['Year'] <= ending_year) & (df['Year'] > starting_year)]
-
 
     filter_Dairy_Product = ['Year',
                             "Hard yellow cheese from cow's milk (100 g)",
@@ -33,8 +32,6 @@ def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, 
                                      'Margarine for spreading in a cup (250 grams)': [],
                                      'Natural yogurt in a plastic container (200 ml)': []})
 
-
-
     groups_by_year = full_relevant_data.groupby('Year')
     for current_year, mini_df_year in groups_by_year:
         print("The current year is: ", current_year)
@@ -49,16 +46,86 @@ def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, 
     percentage_changes.drop('Year', axis=1, inplace=True)  # drop a column
     #percentage_changes.drop(0, inplace=True)  # drop first row
     percentage_changes_format = percentage_changes.applymap(lambda x: f'{x * 100:.1f}')
-    #table_stat_price['Year'] = pd.DataFrame(np.arange(2013, 2023, 1), columns=['Year'])
-    # Create a DataFrame from the range of years
-    year_df = pd.DataFrame(np.arange(2013, 2023, 1), columns=['Year'])
-    # Concatenate along the columns - TODO: not working well!
-    result_df = pd.merge(percentage_changes_format, year_df, on="Year")
-
-    #result_df = pd.concat([percentage_changes_format, list_of_years], axis=1)
+    percentage_changes_format= percentage_changes_format.reset_index()
+    percentage_changes_format = percentage_changes_format.drop(index=0).reset_index(drop=True)
+    percentage_changes_format_table = percentage_changes_format.drop(columns=['index'])
+    percentage_changes_format_table = percentage_changes_format_table.apply(pd.to_numeric)
     print('*')
 
-    return result_df
+    return percentage_changes_format_table
+
+# **************************************************************************************************************
+# Function  name: creating_advance_heatmap
+# input:
+# return value:
+# ****************************************************************************************************************
+def creating_advance_heatmap(data):
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 9))
+
+    sns.set_style('white')
+    sns.heatmap(data, linewidth=3.5, annot=True, square=False, ax=ax1, cmap='Greens', fmt=".1%",
+                    cbar_kws={'label': 'Percentage'}, annot_kws={'weight': 'bold', 'size': 10})
+
+    min_columns = data.idxmin(axis=1)
+    min_columns_colors = ['limegreen' if col == data.columns.get_loc(min_col) else 'none' for col, min_col in zip(range(len(data)), min_columns)]
+    sns.heatmap(data, mask=data != data.min(axis=1, keepdims=True), annot=True, lw=2, linecolor='black',
+                    clip_on=False,
+                    cmap=ListedColormap(min_columns_colors), cbar=False, ax=ax2, fmt=".1%",
+                    cbar_kws={'label': 'Percentage'},
+                    annot_kws={'weight': 'bold', 'size': 10})
+
+    # Customize x-axis and y-axis labels
+    x_labels = ["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)",
+                    'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)']
+    y_labels = ['Year {}'.format(i) for i in range(2009, 2023)]
+
+    ax2.set_xticklabels(x_labels, rotation=45, ha='right', weight='bold', color='darkgreen')
+    ax2.set_yticklabels(y_labels, rotation=0, weight='bold', color='darkgreen')
+
+    ax1.set_xticklabels(x_labels, rotation=45, ha='right', weight='bold', color='darkgreen')
+    ax1.set_yticklabels(y_labels, rotation=0, weight='bold', color='darkgreen')
+
+    ax1.set_title('Variation in Dairy Product Prices\nOver the Past 15 Years', fontsize=20, fontname='Franklin Gothic Medium Cond')
+    plt.tight_layout()
+
+    plt.savefig('heatmap_Advance_chart.jpg', dpi=250, bbox_inches='tight')
+    plt.show()
+
+
+    # fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 9))
+    #
+    # sns.set_style('white')
+    # sns.heatmap(data, linewidth=3.5, annot=True, square=False, ax=ax1, cmap='Grays', fmt=".1%",
+    #             cbar_kws={'label': 'Percentage'}, annot_kws={'weight': 'bold', 'size': 10})
+    #
+    # for ind, row in enumerate(data):
+    #     min_col = np.argmin(row)
+    #     ax1.add_patch(plt.Rectangle((min_col, ind), 1, 1, fc='none', ec='limegreen', lw=3.5, clip_on=False))
+    # sns.heatmap(data, mask=data != data.min(axis=1, keepdims=True), annot=True, lw=2, linecolor='black', clip_on=False,
+    #             cmap=ListedColormap(['limegreen']), cbar=False, ax=ax2, fmt=".1%", cbar_kws={'label': 'Percentage'},
+    #             annot_kws={'weight': 'bold', 'size': 10})
+    # # for idx in len(np.arange(0,2,1)):
+    # ax2.set_xticklabels(["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)",
+    #                      'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)'],
+    #                     rotation=45, ha='right', weight='bold', color='darkgreen')
+    # ax2.set_yticklabels(['Year {}'.format(i) for i in range(2009, 2023)], rotation=0, weight='bold',
+    #                     color='darkgreen')  # Customize y-axis labels
+    #
+    # ax1.set_xticklabels(["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)",
+    #                      'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)'],
+    #                     rotation=45, ha='right', weight='bold', color='darkgreen')
+    # ax1.set_yticklabels(['Year {}'.format(i) for i in range(2009, 2023)], rotation=0, weight='bold',
+    #                     color='darkgreen')  # Customize y-axis labels
+    # #
+    # # # Adjust subplot layout to make room for upper labels
+    # # plt.subplots_adjust(top=0.9)
+    # ax1.set_title('Variation in Dairy Product Prices\nOver the Past 15 Years', fontsize=20,
+    #               fontname='Franklin Gothic Medium Cond')
+    # ax2.set_title('Upward Shift in Dairy Product Prices', fontsize=20, fontname='Franklin Gothic Medium Cond')
+    # plt.tight_layout()
+    # plt.savefig('heatmap_Advance_chart.jpg', dpi=250, bbox_inches='tight')
+    # plt.show()
 
 
 if __name__ == '__main__':
@@ -74,6 +141,7 @@ if __name__ == '__main__':
     starting_year = 2007
 
     result_df = creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, ending_year, starting_year)
+    creating_advance_heatmap(result_df)
     print('*')
 
 
