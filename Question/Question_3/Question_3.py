2c086f4db09da6a9c82715384910f1473e0ce2a9
AnalyticsForPleasure <shaycohen2003@gmail.com>
2024-01-21 01:26:20 +0200

    fixing the bug in the code

--- Question/Question_3/Question_3.py
+++ Question/Question_3/Question_3.py
@@ -4,13 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+
 # **************************************************************************************************************
 # Function  name: creating_the_data_food_item_prices_by_percentage_change_range_of_years
 # input:
 # return value:
 # ****************************************************************************************************************
 def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, ending_year, starting_year):
-
     filter_data_by_year = df.loc[(df['Year'] <= ending_year) & (df['Year'] > starting_year)]
 
     filter_Dairy_Product = ['Year',
@@ -21,10 +21,11 @@ def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, 
                             'Margarine for spreading in a cup (250 grams)',
                             'Natural yogurt in a plastic container (200 ml)']
 
-    column_indices = [filter_data_by_year.columns.get_loc(col) for col in filter_Dairy_Product]  # filter_vegi_and_fruits
+    column_indices = [filter_data_by_year.columns.get_loc(col) for col in
+                      filter_Dairy_Product]  # filter_vegi_and_fruits
     full_relevant_data = filter_data_by_year.iloc[:, column_indices]
     print('*')
-    table_stat_price = pd.DataFrame({'Year':[],
+    table_stat_price = pd.DataFrame({'Year': [],
                                      "Hard yellow cheese from cow's milk (100 g)": [],
                                      'Leben (200 ml)': [],
                                      "Cottage cheese' (250 grams)": [],
@@ -44,15 +45,17 @@ def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, 
     table_stat_price = table_stat_price.apply(pd.to_numeric)
     percentage_changes = table_stat_price.pct_change()
     percentage_changes.drop('Year', axis=1, inplace=True)  # drop a column
-    #percentage_changes.drop(0, inplace=True)  # drop first row
+    # percentage_changes.drop(0, inplace=True)  # drop first row
     percentage_changes_format = percentage_changes.applymap(lambda x: f'{x * 100:.1f}')
-    percentage_changes_format= percentage_changes_format.reset_index()
+    percentage_changes_format = percentage_changes_format.reset_index()
     percentage_changes_format = percentage_changes_format.drop(index=0).reset_index(drop=True)
     percentage_changes_format_table = percentage_changes_format.drop(columns=['index'])
     percentage_changes_format_table = percentage_changes_format_table.apply(pd.to_numeric)
-    print('*')
+    # Reversing the order of the rows in the dataframe
+    percentage_changes_format_table = percentage_changes_format_table.iloc[::-1]
 
     return percentage_changes_format_table
+
 
 # **************************************************************************************************************
 # Function  name: creating_advance_heatmap
@@ -60,8 +63,8 @@ def creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, 
 # return value:
 # ****************************************************************************************************************
 def creating_advance_heatmap(data):
-
-    # Convert DataFrame to NumPy array - inorder to countinue running with the chart
+    # data = data.applymap(lambda x: f'{x:.1f}' if pd.notna(x) else '')
+    # Convert DataFrame to NumPy array - in order to continue running with the chart
     numpy_array_data = data.values
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 9))
@@ -69,8 +72,10 @@ def creating_advance_heatmap(data):
     sns.set_style('white')
 
     # Heatmap number 1 :
-    sns.heatmap(numpy_array_data, linewidth=3.5, annot=True, square=False, ax=ax1, cmap='Greens', fmt=".1%",
-                    cbar_kws={'label': 'Percentage'}, annot_kws={'weight': 'bold', 'size': 10})
+    ax11 = sns.heatmap(numpy_array_data, linewidth=3.5, annot=True, square=False, ax=ax1, cmap='Grays', fmt=".2f",
+                       cbar_kws={'label': 'Percentage'}, annot_kws={'weight': 'bold', 'size': 10})
+
+    for t in ax11.texts: t.set_text(t.get_text() + "%")
 
     # Creating the squares for heatmap 1
     for ind, row in enumerate(numpy_array_data):
@@ -80,29 +85,35 @@ def creating_advance_heatmap(data):
     # Explanation for heatmap 2 :
     # This this code is creating a heatmap where each row is highlighted, showing the minimum value in each row
     # and masking (hiding) the other values. The purpose of the mask is to emphasize the minimum/ maximun value in each row.
-    sns.heatmap(numpy_array_data, mask=numpy_array_data != numpy_array_data.max(axis=1, keepdims=True), annot=True, lw=2, linecolor='black', clip_on=False,
-                cmap=ListedColormap(['limegreen']), cbar=False, ax=ax2, fmt=".1%",
-                cbar_kws={'label': 'Percentage'},
-                annot_kws={'weight': 'bold', 'size': 10})
+    ax22 = sns.heatmap(numpy_array_data, mask=numpy_array_data != numpy_array_data.max(axis=1, keepdims=True),
+                       annot=True, lw=2, linecolor='black', clip_on=False,
+                       cmap=ListedColormap(['limegreen']), cbar=False, ax=ax2, fmt=".2f",
+                       cbar_kws={'label': 'Percentage'},
+                       annot_kws={'weight': 'bold', 'size': 10})
+
+    for t in ax22.texts: t.set_text(t.get_text() + "%")
 
     # Customize x-axis and y-axis labels
     x_labels = ["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)",
-                    'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)']
-    y_labels = ['Year {}'.format(i) for i in range(2009, 2023)]
+                'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)']
+    y_labels = ['Year {}'.format(i) for i in range(2009, 2023)][::-1]
 
     # loop iterates over the two subplots (ax1 and ax2)
     for ax in [ax1, ax2]:
         ax.set_xticklabels(x_labels, rotation=45, ha='right', weight='bold', color='darkgreen')
         ax.set_yticklabels(y_labels, rotation=0, weight='bold', color='darkgreen')
 
-    ax1.set_title('Variation in Dairy Product Prices\nOver the Past 15 Years', fontsize=20, fontname='Franklin Gothic Medium Cond')
+    ax1.set_title('The annual variance of Dairy Product\nPrices over a 15-year period', fontsize=20,
+                  fontname='Franklin Gothic Medium Cond')
+    ax2.set_title('Which product experienced the highest\nprice growth each year?', fontsize=20,
+                  fontname='Franklin Gothic Medium Cond')
+
     plt.tight_layout()
     plt.savefig('heatmap_Advance_chart.jpg', dpi=250, bbox_inches='tight')
     plt.show()
 
 
 if __name__ == '__main__':
-
     pd.set_option('display.max_rows', 5000)
     df = pd.read_csv('/home/shay_diy/PycharmProjects/digging_deep_into_Israel_prices/Data/israel_prices.csv')
     df = df.replace(np.nan, '', regex=True)
@@ -116,7 +127,3 @@ if __name__ == '__main__':
     result_df = creating_the_data_food_item_prices_by_percentage_change_range_of_years_(df, ending_year, starting_year)
     creating_advance_heatmap(result_df)
     print('*')
-
-
-
-
--- Question/Question_3/preparation_for_the_chart.py
+++ Question/Question_3/preparation_for_the_chart.py
@@ -6,26 +6,39 @@ from matplotlib.colors import ListedColormap
 data = np.random.uniform(-0.193, 0.082, size=(14, 6)) # very nice - I didn't know that!
 
 
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 9))
-sns.set_style('white')
+#data = np.random.uniform(-0.193, 0.082, size=(10, 6))
 
-sns.heatmap(data, linewidth=3.5, annot=True,square=False, ax=ax1, cmap = 'Grays',fmt=".1%",cbar_kws={'label': 'Percentage'},annot_kws={'weight': 'bold', 'size': 10})
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 9))
+sns.set_style('dark')
+
+# Heatmap number 1 :
+sns.heatmap(data, linewidth=3.5, annot=True,square=False, ax=ax1, cmap = 'Grays',fmt=".2%",cbar_kws={'label': 'Percentage'},annot_kws={'weight': 'bold', 'size': 10})
+
+# Creating the squares for heatmap 1
 for ind, row in enumerate(data):
-    min_col = np.argmin(row)
+    min_col = np.argmax(row)
     ax1.add_patch(plt.Rectangle((min_col, ind), 1, 1, fc='none', ec='limegreen', lw=3.5, clip_on=False))
 
-sns.heatmap(data, mask=data != data.min(axis=1, keepdims=True), annot=True, lw=2, linecolor='black', clip_on=False,
-            cmap=ListedColormap(['limegreen']), cbar=False, ax=ax2,fmt=".1%",cbar_kws={'label': 'Percentage'},annot_kws={'weight': 'bold', 'size': 10})
 
-#for idx in len(np.arange(0,2,1)):
-ax2.set_xticklabels(["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)", 'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)'],
-                    rotation=45, ha='right',weight='bold',color = 'darkgreen')
-ax2.set_yticklabels(['Year {}'.format(i) for i in range(2009, 2023)], rotation=0,weight='bold',color = 'darkgreen')  # Customize y-axis labels
+# Name: Explanation for heatmap 2
+# input: This this code is creating a heatmap where each row is highlighted, showing the minimum value in each row
+#        and masking (hiding) the other values. The purpose of the mask is to emphasize the minimum/ maximun value in each row.
+# ***************************************************************************************************************
+sns.heatmap(data, mask=data != data.max(axis=1, keepdims=True), annot=True, lw=2, linecolor='black', clip_on=False,
+            cmap=ListedColormap(['limegreen']), cbar=False, ax=ax2,fmt=".2%",cbar_kws={'label': 'Percentage'},annot_kws={'weight': 'bold', 'size': 10})
+
+x_labels = ["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)", 'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)']
+y_labels = ['Year {}'.format(i) for i in range(2009, 2023)]
 
 
-ax1.set_xticklabels(["Hard yellow cheese\n(100 g)", 'Leben\n(200 ml)', "Cottage cheese'\n(250 grams)", 'Soft white cheese\n(250 grams)', 'Margarine\n(250 grams)', 'Natural yogurt\n(200 ml)'],
-                    rotation=45, ha='right', weight='bold',color = 'darkgreen')
-ax1.set_yticklabels(['Year {}'.format(i) for i in range(2009, 2023)], rotation=0,weight='bold',color = 'darkgreen')  # Customize y-axis labels
+# loop iterates over the two subplots (ax1 and ax2)
+for ax in [ax1, ax2]:
+    ax.set_xticklabels(x_labels, rotation=45, ha='right', weight='bold', color='darkgreen')
+    ax.set_yticklabels(y_labels, rotation=0, weight='bold', color='darkgreen')
+
+
+
+
 
 ax1.set_title('Variation in Dairy Product Prices\nOver the Past 15 Years', fontsize=20, fontname='Franklin Gothic Medium Cond')
 ax2.set_title('Upward Shift in Dairy Product Prices', fontsize=20, fontname='Franklin Gothic Medium Cond')
@@ -33,5 +46,10 @@ ax2.set_title('Upward Shift in Dairy Product Prices', fontsize=20, fontname='Fra
 plt.tight_layout()
 
 plt.savefig('heatmap_Advance_chart.jpg', dpi=250, bbox_inches='tight')
+
+
+# Adding picture - https://www.kaggle.com/code/andradaolteanu/bitcoin-dogecoin-on-rapids-and-elon-musk
+#path='../input/all-elon-musks-tweets/images/images/elon_rocket.png'
+#offset_png(x=6.9, y=2000, path=path, ax=ax, zoom=0.27, offset=0)
 plt.show()
 
